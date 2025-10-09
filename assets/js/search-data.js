a68b37d73191689b3f33b34294766e32b528ae81
betive37 <betive37@gmail.com>
2025-10-09 09:06:34 +0000

    Deploying to gh-pages from @ betive37/betive37.github.io@cd8147914293c5aa2b976c479aa727c17bddbe3a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,9 +44,9 @@ ninja.data = [{
           description: "at ISMIR 2025 (LBD)",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project.html";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-cake-able-diffusion",
+          title: 'Cake-able Diffusion',
+          description: "Team Project on Generative AI - AATG010",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project.html";
             },},{id: "projects-project-3-with-very-long-name",
